b356e8872277d6485bec60560ff6794e8ab0fe3c
NgoMinhKhoi2603 <109575681+NgoMinhKhoi2603@users.noreply.github.com>
2022-09-21 21:24:12 +0700

    up

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import {Component} from "@angular/core";
+import {IStudent} from "../../interfaces/student.interfaces";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
 export class HomeComponent extends Component{
-
+  student: IStudent = {id:1,name:'Nam',age:17,address:'8A Ton That Thuyet'}
 }
